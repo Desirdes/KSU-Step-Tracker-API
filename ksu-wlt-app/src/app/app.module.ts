e6e6d82f9b47296f2ffd6743bd42bda153e19d2d
Santhiya S <ssubram6@students.kennesaw.edu>
2023-02-14 16:16:50 -0500

    user

--- ksu-wlt-app/src/app/app.module.ts
+++ ksu-wlt-app/src/app/app.module.ts
@@ -1,26 +1,47 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatInputModule} from '@angular/material/input';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatTabsModule } from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartComponent } from './chart/chart.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserProfileComponent
+    UserProfileComponent,
+    ChartComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    NgbModule
+    NgbModule,
+    MatInputModule,
+    MatTabsModule,
+    NgChartsModule,
+    MatFormFieldModule,
+    MatGridListModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class AppModule { }
--- ksu-wlt-app/src/app/user-profile/user-profile.component.ts
+++ ksu-wlt-app/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Chart } from 'node_modules/chart.js';
 
 
 @Component({
@@ -6,5 +7,58 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent {}
+export class UserProfileComponent implements OnInit{
+  
+  public myFinalValue: number;
+  public targetWeight: number;
+  
+  constructor(){}
+  ngOnInit(): void {
+    var myChart = new Chart("myChart", {
+      type: 'bar',
+      data: {
+          labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              y: {
+                  beginAtZero: true
+              }
+          }
+      }
+  });
+  }
+
+  
+  
+  valueUpdated(event) {
+    this.myFinalValue = 100 + +event.target.value;
+  }
+  valueChange(){
+    this.targetWeight = 100 + 100;
+    return this.targetWeight;
+
+  }
+}
 
