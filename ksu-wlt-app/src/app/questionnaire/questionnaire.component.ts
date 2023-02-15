db8b84aca9533487464aef1f363436800a112d44
asfuller <alexfuller123@yahoo.com>
2023-02-15 08:43:31 -0500

    Created questionnaire page

--- ksu-wlt-app/src/app/questionnaire/questionnaire.component.ts
+++ ksu-wlt-app/src/app/questionnaire/questionnaire.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-questionnaire',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./questionnaire.component.css']
 })
 export class QuestionnaireComponent {
-
+  questionnaireForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    age: new FormControl(''),
+    dateOfBirth: new FormControl(''),
+    sex: new FormControl(''),
+    race: new FormControl(''),
+    height: new FormControl(''),
+    weight: new FormControl(''),
+    waistCircumference: new FormControl(''),
+    headCircumference: new FormControl(''),
+    targetWeightLoss: new FormControl('')
+})
 }
+
+/*onSubmit() {
+  // TODO: Use EventEmitter with form value
+  this.router.navigateByUrl('');
+}*/
