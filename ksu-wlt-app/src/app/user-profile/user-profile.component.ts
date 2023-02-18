aba25f7fafa16fd816e8215ff7f62a8666b92a2e
Santhiya S <ssubram6@students.kennesaw.edu>
2023-02-17 23:07:58 -0500

    updated

--- ksu-wlt-app/src/app/user-profile/user-profile.component.ts
+++ ksu-wlt-app/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Chart } from 'node_modules/chart.js';
 
 
@@ -7,58 +8,21 @@ import { Chart } from 'node_modules/chart.js';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
-export class UserProfileComponent implements OnInit{
-  
-  public myFinalValue: number;
-  public targetWeight: number;
-  
-  constructor(){}
-  ngOnInit(): void {
-    var myChart = new Chart("myChart", {
-      type: 'bar',
-      data: {
-          labels: ['week1', 'week2', 'week3', 'week4', 'week5', 'week6'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              y: {
-                  beginAtZero: true
-              }
-          }
-      }
-  });
+export class UserProfileComponent{
+  isDisabled:boolean = true;
+  // @ViewChild('f') editForm: NgForm
+  // onSubmit(form: NgForm){
+  //   this.editForm.value.username
+  // }
+
+  edit(){
+    this.isDisabled=false
+  }
+
+  update(){
+    this.isDisabled= true
   }
 
   
-  
-  valueUpdated(event) {
-    this.myFinalValue = 100 + +event.target.value;
-  }
-  valueChange(){
-    this.targetWeight = 100 + 100;
-    return this.targetWeight;
-
-  }
 }
 
