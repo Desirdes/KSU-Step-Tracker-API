cb133b7f0cf34fa9d88c23a631c1fb9b38257cbf
David <dsanfor5@students.kennesaw.edu>
2023-03-01 20:27:21 -0500

    David - Added Email support

---
+++ src/main/java/com/ksupwlt/stepcounttracker/controller/EmailController.java
@@ -0,0 +1,35 @@
+package com.ksupwlt.stepcounttracker.controller;
+
+import com.ksupwlt.stepcounttracker.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
+
+@RestController
+public class EmailController {
+    @Autowired
+    private EmailService emailService;
+
+    public static class EmailDetails {
+        public String recipient;
+        public String subject;
+        public String content;
+    }
+
+    @PostMapping(
+            value = "/sendEmail",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public void sendEmail(@RequestBody EmailDetails emailDetails) {
+        try {
+            emailService.sendEmail(emailDetails.recipient, emailDetails.subject, emailDetails.content);
+        } catch (UnsupportedEncodingException | MessagingException e) {
+            System.out.println(e.getStackTrace());
+        }
+    }
+}
