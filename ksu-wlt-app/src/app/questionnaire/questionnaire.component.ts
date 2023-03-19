f1989d40a7e6318416595b5ea6d2aa182f3cbe84
asfuller <alexfuller123@yahoo.com>
2023-03-19 17:00:36 -0400

    All components updated

--- ksu-wlt-app/src/app/questionnaire/questionnaire.component.ts
+++ ksu-wlt-app/src/app/questionnaire/questionnaire.component.ts
@@ -13,8 +13,6 @@ integerRegex = /^\d+$/
 integerRegexDecimal = /^\d*\.?\d*$/
 
   questionnaireForm = this.fb.group({
-    firstName: ['', [Validators.required]],
-    lastName: ['', [Validators.required]],
     age: ['', [Validators.required, Validators.max(39), Validators.min(19), Validators.pattern(this.integerRegex)]],
     sex: ['', [Validators.required]],
     race: ['', [Validators.required]],
@@ -32,8 +30,7 @@ getControl(name:any) : AbstractControl | null {
   
 }
 
-/*get firstName() { return this.questionnaireForm.get('firstName'); }
-get lastName() { return this.questionnaireForm.get('lastName'); }
+//For backend
 get age() { return this.questionnaireForm.get('age'); }
 get dateOfBirth() { return this.questionnaireForm.get('dateOfBirth'); }
 get sex() { return this.questionnaireForm.get('sex'); }
@@ -43,7 +40,7 @@ get weight() { return this.questionnaireForm.get('weight'); }
 get waistCircumference() { return this.questionnaireForm.get('waistCircumference'); }
 get neckCircumference() { return this.questionnaireForm.get('neckCircumference'); }
 get targetWeightLossPercentage() { return this.questionnaireForm.get('targetWeightLossPercentage'); }
-get bodyFatPercentage() { return this.questionnaireForm.get('bodyFatPercentage'); }*/
+get bodyFatPercentage() { return this.questionnaireForm.get('bodyFatPercentage'); }
 
 constructor(private fb: FormBuilder, private router: Router) { }
 
@@ -72,6 +69,7 @@ public calcTargetWeight() { //This is correct
   var currentWeight = +this.questionnaireForm.value.weight!/this.lbsConversionToKG;
   var targetWeightLoss = (+this.questionnaireForm.value.targetWeightLossPercentage!/100) + this.percentageConversionToKG;
   var targetWeight = currentWeight - targetWeightLoss;
+  targetWeight.toFixed(2)
   this.targetWeight = targetWeight;
 }
 
@@ -82,16 +80,19 @@ public calcTargetBodyFatPercentage() { //This needs work; it may be right but we
   var newFatMass = currentFatMass - targetWeightLoss;
   var targetWeight = (+this.questionnaireForm.value.weight!/this.lbsConversionToKG) - targetWeightLoss; 
   var targetBodyFatPercentage = (newFatMass / targetWeight) * 100;
+  targetBodyFatPercentage.toFixed(2)
   this.targetBodyFatPercentage = targetBodyFatPercentage;
 }
 
 public calcSteps() { //This is starting to get right, but the above is wrong
   if(this.questionnaireForm.value.sex == "Male") {
     var stepsPerPoundPerDay =  39377.34 / (this.targetBodyFatPercentage)^1.3405;
+    stepsPerPoundPerDay.toFixed(0);
     this.stepsPerDay = stepsPerPoundPerDay * ((+this.questionnaireForm.value.weight!/this.lbsConversionToKG) * (+this.questionnaireForm.value.bodyFatPercentage!/100));
   }
   else if (this.questionnaireForm.value.sex == "Female") {
     var stepsPerPoundPerDay =  261425.4 / (this.targetBodyFatPercentage)^1.8797;
+    stepsPerPoundPerDay.toFixed(0);
     this.stepsPerDay = stepsPerPoundPerDay * ((+this.questionnaireForm.value.weight!/this.lbsConversionToKG) * (+this.questionnaireForm.value.bodyFatPercentage!/100));
   }
 }
