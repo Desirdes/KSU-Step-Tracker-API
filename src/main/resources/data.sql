9da0262e5bd6102f93e292d279874ea02dc4c730
David Sanford <55630963+Desirdes@users.noreply.github.com>
2023-04-30 21:07:05 -0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 -- Create Admin
-INSERT INTO users (id, username, password, roles, groupid, personid, reset_password) VALUES
-(1, 'admin', '$2a$10$hawhRLPtIj/mmZdD7/2Og.CzcvrkKY3VNPRHa1ZVlBOfK3gLyLCaK','ROLE_ADMIN', null, null, false);
+INSERT INTO users (username, password, roles, groupid, personid, reset_password) VALUES
+('admin', '$2a$10$hawhRLPtIj/mmZdD7/2Og.CzcvrkKY3VNPRHa1ZVlBOfK3gLyLCaK','ROLE_ADMIN', null, null, false);
